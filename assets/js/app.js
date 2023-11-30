db6497fd1873def6675d2eb296781a918527c4f2
Rohit-Dhaka <rohitdhaka2110@gamil.com>
2023-11-30 11:21:47 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,64 @@
+// --------nav-bar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+// -------slick-slider
+$('.slick-cards').slick({
+    dots: true,
+    infinite: true,
+    speed: 900,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay:true,    
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }                  
+    ]
+  });
+// ------------top-arrow   
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// -----------dark-mode
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
